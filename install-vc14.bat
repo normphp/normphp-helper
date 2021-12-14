f195c4fa6813b6c8508f8519992a9f20ce9967c7
pizepei <pizepei@pizepei.com>
2021-12-14 22:48:14 +0800

    优化项目

--- install-vc14.bat
+++ install-vc14.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 setlocal DISABLEDELAYEDEXPANSION
 echo *==========================The execution environment=================================*
-php\8.1\php.exe -v
+php\base\php.exe -v
 echo *==========================install vc14 =================================*
 pause
-php\8.1\php.exe execute.php install-vc14
+php\base\php.exe execute.php install-vc14
 pause
--- update-normphp.bat
+++ update-normphp.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 setlocal DISABLEDELAYEDEXPANSION
 echo *==========================The execution environment=================================*
-php\8.1\php.exe -v
+php\base\php.exe -v
 echo *==========================update cli normphp =================================*
 pause
-php\8.1\php.exe execute.php update
+php\base\php.exe execute.php update
 pause
